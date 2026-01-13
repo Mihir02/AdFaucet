ded0e58157d6011c2f8b0aa29e05395740529c54
Mihir Upadhyay <varahamihira2@gmail.com>
2026-01-14 01:48:40 +0530

    my bad again

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10459773
+    "zoneId": 10459770
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
